80f474baeeab198f0c15397421c7a1ca53f1c755
alwod <mistergeralt@gmail.com>
2023-11-21 14:28:25 +0300

    GameCharacter servlet ready and so is everything

--- app/src/main/java/homework/models/GameCharacter.java
+++ app/src/main/java/homework/models/GameCharacter.java
@@ -5,11 +5,20 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.List;
+
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class GameCharacter {
+    private Long id;
+    private Long weaponId;
     private String name;
     private Integer powerLevel;
+    private List<Long> factionIds;
+
+    public GameCharacter(Long id, Long weaponId, String name, Integer powerLevel) {
+        this(id, weaponId, name, powerLevel, null);
+    }
 }
